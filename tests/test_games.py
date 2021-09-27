fa9adffb98adc0eda25f18024b5621da03ce4313
pcygan44 <p.cygan44@gmail.com>
2021-09-27 09:33:52 +0100

    commit 5

--- tests/test_games.py
+++ tests/test_games.py
@@ -6,19 +6,19 @@ from models.player import Player
 
 class TestGames(unittest.TestCase):
 
-    def setUp(self, ):
+    def setUp(self):
         self.player1 = Player ("Jim", "paper" )
         self.player2 = Player ("Tom", "rock")
         self.player3 = Player ("Gil", "paper")
 
     def test_game_draw(self):
-        resoult = game(self, self.player1, self.player3)
+        resoult = game(self.player1, self.player3)
         self.assertEqual(None,resoult)
 
 
     def test_game_player1_win(self):
-        result = game(self, self.player1, self.player2)
+        result = game(self.player1, self.player2)
         self.assertEqual("Player1 wins with paper", result)
 
     def test_game_palyer2_win(self):
-        result = game(self, self.player1, self.player2)
+        result = game(self.player1, self.player2)
